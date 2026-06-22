2666323a2c8d896a517304a0116961fa674b066b
Nayely <nayeluc2006@gmail.com>
2026-06-21 23:28:45 -0500

    Agregar proyecto de pruebas unitarias

---
+++ Monolito_4bm.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Monolito_4bm.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Monolito_4bm.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35481c84-8e31-4bda-b9f6-26f0d44d7116")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Monolito_4bm.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Monolito_4bm.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_ConexionBaseDatos_SQLServer()
+        {
+            // Prueba de conexión simulada a la base de datos SQL Server
+            bool conexionEstablecida = true;
+            Assert.IsTrue(conexionEstablecida, "La conexión a SQL Server debería haberse establecido correctamente.");
+        }
+
+        [TestMethod]
+        public void Test_CalculoPrecios_Productos()
+        {
+            // Prueba de lógica de negocio básica (ej. cálculo de IVA/impuestos en productos)
+            double precioProducto = 100.0;
+            double ivaEsperado = 15.0; // 15% de IVA
+            double ivaCalculado = precioProducto * 0.15;
+
+            Assert.AreEqual(ivaEsperado, ivaCalculado, "El cálculo del IVA en el precio del producto falló.");
+        }
+    }
+}
